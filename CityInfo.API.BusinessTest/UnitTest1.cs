e8ce9ec1d6ed3910b9983ace25974ca08200702a
dorina-adina <adinacapot10@gmail.com>
2024-12-06 10:02:00 +0200

    Adjusted layered arhitecture project

---
+++ CityInfo.API.BusinessTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CityInfo.API.BusinessTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ CityInfo.API.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CityInfo.API.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
