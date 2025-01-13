ca28af673d4bee45efb9a631d456aec3a168225c
dorina-adina <146988833+dorina-adina@users.noreply.github.com>
2025-01-13 12:55:18 +0200

    am-recuperat-data-access

---
+++ ToDoList.API.Data_AccessLayer/Entities/ToDoList.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList.Api.DataAccess_Layer.Entities
+{
+    public class ToDoList
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Task { get; set; }
+        public short Priority { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+
+    }
+}
---
+++ ToDoList.API.Data_AccessLayer/Services/IMailService.cs
@@ -0,0 +1,7 @@
+﻿namespace CityInfo.API.Businsess_Layer.Services
+{
+    public interface IMailService
+    {
+        void Send(string subject, string message);
+    }
+}
